14dca90d655b297143369f8fc53e355f351ac811
Khadim <khadim.khouma.pro@gmail.com>
2018-12-20 15:24:21 +0000

    Configure app name

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <footer class="text-center">
-        <p>&copy; <?php echo date('Y')?> &middot; Laracarte by <a href="http://twitter.com/etsmo">@etsmo</a> - A <a href="https://laramap.com" target="_blank">Laramap</a> clone app.</p>
+        <p>&copy; <?php echo date('Y')?> &middot; {{ config('app.name') }} by <a href="http://twitter.com/etsmo">@etsmo</a> - A <a href="https://laramap.com" target="_blank">Laramap</a> clone app.</p>
         <p class="text-center"><b>This app has been built for learning purposes.</b></p>
     </footer>
 </div>
